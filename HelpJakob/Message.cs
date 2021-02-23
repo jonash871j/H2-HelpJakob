91ec04ead7ac04419c3955b56e0d4c707dcba208
Jonas hofmann <Jonash871j@gmail.com>
2021-02-23 09:21:52 +0100

    Commit

--- HelpJakob/Message.cs
+++ HelpJakob/Message.cs
@@ -2,16 +2,14 @@
 {
     public class Message
     {
-        public Message(string to, string from, string body, string subject, string cc)
+        public Message(string from, string body, string subject, string cc)
         {
-            To = to;
             From = from;
             Body = body;
             Subject = subject;
             Cc = cc;
         }
 
-        public string To { get; set; }
         public string From { get; set; }
         public string Body { get; set; }
         public string Subject { get; set; }
